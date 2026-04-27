512a3d7a7ecf4e0ac4019afc32e6f1249c145c0d
Mat <mjadams421@gmail.com>
2026-04-27 21:29:15 +0000

    Added to page

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("info").addEventListener("click", function() { 
+alert  ("For more information on the vehicles please see links below");
+};
+
+let update = document.getElementById("curent");
+
+function Add() {
+update.innerHTML= "Car_Suzuki Kizashi, Quad_ Honda Recon, 
+Bike_ Cannondale Trail"
+};
+
+document.addEventListener("click", Add);
