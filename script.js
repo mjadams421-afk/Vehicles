2437519e67444b210de382b5b70f482f4bee7376
Mat <mjadams421@gmail.com>
2026-04-30 16:11:34 +0000

    Added to JS functions

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+//Setting variables
 const news = document.getElementById("info")
-
 const update = document.getElementById("curent");
-
+const reset = document.getElementById("reset");
+//Functions with variables
 function Add() {
 update.innerHTML= "Car_Suzuki Kizashi, Quad_ Honda Recon, Bike_ Cannondale Trail";
+update.style.fontSize = "1.3rem";
+update.style.color = "blue";
 }
-
 function Plus() {
 news.innerHTML= "All new information is below";
+update.style.fontSize = "1.3rem";
+update.style.color = "red";
 }
-
 
 update.addEventListener("mousedown", Add);
 news.addEventListener("mousedown", Plus);
+reset.removeEventListener("mousedown", Add);
+reset.removeEventListener("mousedown", Plus);
