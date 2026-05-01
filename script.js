880c572efc067f55cb1a0bda1c6438ac733cc78a
Mat <mjadams421@gmail.com>
2026-05-01 14:44:17 +0000

    Modified JS code

--- script.js
+++ script.js
@@ -2,23 +2,26 @@
 const news = document.getElementById("info")
 const update = document.getElementById("curent");
 const reset = document.getElementById("reset");
+
 //Functions with variables
 function Add() {
 update.innerHTML= "Car_Suzuki Kizashi, Quad_ Honda Recon, Bike_ Cannondale Trail";
 update.style.fontSize = "1.3rem";
 update.style.color = "blue";
+document.getElementById("textA").innerHTML = "Text was displayed for vehicles";
 }
 function Plus() {
 news.innerHTML= "All new information is below";
 update.style.fontSize = "1.3rem";
 update.style.color = "red";
+document.getElementById("textB").innerHTML = "Text was displayed for information";
 }
 function Reset() { 
-update.removeEventListener("mousedown", Add);
-news.removeEventListener("mousedown", Plus);
+update.innerHTML= "Click to get information";
+news.innerHTML= "Click for current vehicles";
 }
-//Event Listeners
 
+//Event Listeners
 update.addEventListener("mousedown", Add);
 news.addEventListener("mousedown", Plus);
 
