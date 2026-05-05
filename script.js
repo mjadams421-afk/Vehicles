587d6e33e37c2a041d0882a8cad051c09ce0f351
Mat <mjadams421@gmail.com>
2026-05-05 03:07:55 +0000

    Added to JS changes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const news = document.getElementById("info")
 const update = document.getElementById("curent");
 const reset = document.getElementById("reset");
+const log = document.getElementById("textInfo");
 //Random number generator
 let X = Math.floor(Math.random() * 100);
 let Y = '';
@@ -10,6 +11,7 @@ else if (X > 10 && X <= 80)
 { Y = 'Number is Good interval'} 
 else { Y = 'Number is high'}
 let Z = ["Quad",1,"Car",1,"Bike",2];
+Z.join(" - ");
 
 //Functions with variables
 function Add() {
@@ -17,6 +19,8 @@ update.innerHTML= "Car_Suzuki Kizashi, Quad_ Honda Recon, Bike_ Cannondale Trail
 update.style.fontSize = "1.3rem";
 update.style.color = "blue";
 document.getElementById("textA").innerHTML = "Text was displayed for vehicles";
+document.getElementById("textA").style.fontSize = "1.3rem";
+document.getElementById("textA").style.color = "white";
 }
 function Plus() {
 news.innerHTML= "All new information is below";
@@ -24,15 +28,23 @@ update.style.fontSize = "1.3rem";
 update.style.color = "red";
 document.getElementById("textB").innerHTML = "Text was displayed for information." + " " + "Random number: " + " " 
 + X + " " + Y;
+document.getElementById("textInfo").style.fontSize = "1.3rem";
+document.getElementById("textInfo").style.color = "white";
 }
 function Reset() { 
 update.innerHTML= "Click to get information";
 news.innerHTML= "Click for current vehicles";
 }
+function Log() {
+log.innerHTML= "Vehicle Log" + " " + Z;
+log.style.fontSize = "1.3rem";
+log.style.color = "white";
+}
 
 //Event Listeners
 update.addEventListener("mousedown", Add);
 news.addEventListener("mousedown", Plus);
+log.addEventListener("mousedown", Log);
 
 //RESET button
 reset.addEventListener("mousedown", Reset);
